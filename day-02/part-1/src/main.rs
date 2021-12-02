272b1b43eb4f5036ada47515bc40fc343ed287d5
Miquel van Smoorenburg <mike@langeraar.net>
2021-12-02 12:50:07 +0100

    kan ook iets functioneler

--- day-02/part-1/src/main.rs
+++ day-02/part-1/src/main.rs
@@ -7,17 +7,16 @@ fn main() {
         .lock()
         .lines()
         .map(|line| line.unwrap())
-        .fold((0u32, 0u32), |mut a, line| {
+        .fold((0u32, 0u32), |s, line| {
             let mut i = line.split_whitespace();
             let dir = i.next().unwrap();
             let val: u32 = i.next().unwrap().parse().unwrap();
             match dir {
-                "forward" => a.0 += val,
-                "down" => a.1 += val,
-                "up" => a.1 -= val,
+                "forward" => (s.0 + val, s.1),
+                "down" => (s.0, s.1 + val),
+                "up" => (s.0, s.1 - val),
                 _ => unreachable!(),
-            };
-            a
+            }
         });
     println!("{}", pos.0 * pos.1);
 }
