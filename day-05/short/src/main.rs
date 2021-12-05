7d2d0830abeee16dab59302fffcf2fe5472cac9c
Miquel van Smoorenburg <mike@langeraar.net>
2021-12-05 17:18:37 +0100

    shave off another line

--- day-05/short/src/main.rs
+++ day-05/short/src/main.rs
@@ -4,8 +4,7 @@ use scan_fmt::*;
 fn main() {
     let mut floor = HashMap::<(i16, i16), i16>::default();
     while let Ok((mut x1, mut y1, x2, y2)) = scanln_fmt!("{},{} -> {},{}", i16, i16, i16, i16) {
-        let xd = if x1 > x2 { -1 } else { std::cmp::min(1, x2 - x1) };
-        let yd = if y1 > y2 { -1 } else { std::cmp::min(1, y2 - y1) };
+        let (xd, yd) = ((x2 - x1).signum(), (y2 - y1).signum());
         loop {
             *floor.entry((x1, y1)).or_insert(0) += 1;
             if x1 == x2 && y1 == y2 {
