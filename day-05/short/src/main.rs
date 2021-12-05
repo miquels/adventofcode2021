ae50a64d84b11f7a39c9001a5f86d23d857778a5
Miquel van Smoorenburg <mike@langeraar.net>
2021-12-05 17:37:10 +0100

    code golf

--- day-05/short/src/main.rs
+++ day-05/short/src/main.rs
@@ -3,16 +3,13 @@ use scan_fmt::*;
 
 fn main() {
     let mut floor = HashMap::<(i16, i16), i16>::default();
-    while let Ok((mut x1, mut y1, x2, y2)) = scanln_fmt!("{},{} -> {},{}", i16, i16, i16, i16) {
-        let (xd, yd) = ((x2 - x1).signum(), (y2 - y1).signum());
-        loop {
-            *floor.entry((x1, y1)).or_insert(0) += 1;
-            if x1 == x2 && y1 == y2 {
-                break;
-            }
-            x1 += xd;
-            y1 += yd;
-        }
+    while let Ok(mut c) = scanln_fmt!("{},{} -> {},{}", i16, i16, i16, i16) {
+        let (xd, yd) = ((c.2 - c.0).signum(), (c.3 - c.1).signum());
+        while {
+            *floor.entry((c.0, c.1)).or_insert(0) += 1;
+            c = (c.0 + xd, c.1 + yd, c.2, c.3);
+            c.0 - xd != c.2 || c.1 - yd != c.3
+        } {}
     }
     println!("part 2: {}", floor.values().filter(|&&n| n >= 2).count());
 }
