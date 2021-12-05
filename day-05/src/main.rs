5cbd8952e0e5dbe9b5991aadfa4876b40b590a96
Miquel van Smoorenburg <mike@langeraar.net>
2021-12-05 15:08:34 +0100

    use a hashmap instead of an array

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,23 +1,19 @@
 // cargo run < ../input/input.txt
 
+use std::collections::HashMap;
 use std::io::{self, BufRead};
-use std::cmp::max;
 
-struct Floor(Vec<Vec<u16>>);
+#[derive(Default)]
+struct Floor(HashMap<(i16, i16), u16>);
 
 impl Floor {
-    fn new(max_x: usize, max_y: usize) -> Floor {
-        let v = (0..max_y).map(|_| { let mut v = Vec::new(); v.resize(max_x, 0u16); v }).collect();
-        Floor(v)
-    }
-
     fn draw(&mut self, line: &Line) {
         let dir = |i, j| if i > j { -1 } else { std::cmp::min(1, j - i) };
         let xd = dir(line.x1, line.x2);
         let yd = dir(line.y1, line.y2);
         let (mut x, mut y) = (line.x1, line.y1);
         loop {
-            self.0[y as usize][x as usize] += 1;
+            *self.0.entry((y, x)).or_insert(0) += 1;
             if x == line.x2 && y == line.y2 {
                 break;
             }
@@ -27,7 +23,7 @@ impl Floor {
     }
 
     fn ndanger(&self) -> usize {
-        self.0.iter().map(|y| y.iter()).flatten().filter(|&&n| n >= 2).count()
+        self.0.values().filter(|&&n| n >= 2).count()
     }
 }
 
@@ -56,14 +52,11 @@ fn main() {
         .lines()
         .map(|s| Line::parse(&s.unwrap()))
         .collect::<Vec<_>>();
-    let max_x = lines.iter().map(|l| max(l.x1, l.x2) as usize + 1).max().unwrap();
-    let max_y = lines.iter().map(|l| max(l.y1, l.y2) as usize + 1).max().unwrap();
-
-    let mut floor = Floor::new(max_x, max_y);
+    let mut floor = Floor::default();
     lines.iter().filter(|l| l.x1 == l.x2 || l.y1 == l.y2).for_each(|l| floor.draw(&l));
     println!("part 1: {}", floor.ndanger());
 
-    floor = Floor::new(max_x, max_y);
+    floor = Floor::default();
     lines.iter().for_each(|l| floor.draw(&l));
     println!("part 2: {}", floor.ndanger());
 }
