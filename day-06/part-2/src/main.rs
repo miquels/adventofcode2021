52b67a1f483004e3e7e6074a87c4839e652e3c45
Miquel van Smoorenburg <mike@langeraar.net>
2021-12-06 14:27:26 +0100

    use array and rotate_left()

--- day-06/part-2/src/main.rs
+++ day-06/part-2/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
     let mut line = String::new();
     std::io::stdin().read_line(&mut line).unwrap();
-    let mut state = [0u64; 9].to_vec();
+    let mut state = [0u64; 9];
     line.trim().split(',').map(|v| v.parse::<usize>().unwrap()).for_each(|v| state[v] += 1);
     for _ in 0 .. 256 {
-        let n = state.remove(0);
-        state[6] += n;
-        state.push(n);
+        state.rotate_left(1);
+        state[6] += state[8];
     }
-    println!("{}", state.drain(..).sum::<u64>());
+    println!("{}", state.iter().sum::<u64>());
 }
