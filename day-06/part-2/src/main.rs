06ed92fa38f6a21f7b67509ebdfc6d0b3e01a3e2
Miquel van Smoorenburg <mike@langeraar.net>
2021-12-06 17:01:04 +0100

    code golf

--- day-06/part-2/src/main.rs
+++ day-06/part-2/src/main.rs
@@ -3,9 +3,6 @@ fn main() {
     std::io::stdin().read_line(&mut line).unwrap();
     let mut state = [0u64; 9];
     line.trim().split(',').map(|v| v.parse::<usize>().unwrap()).for_each(|v| state[v] += 1);
-    for _ in 0 .. 256 {
-        state.rotate_left(1);
-        state[6] += state[8];
-    }
+    (1..=256).map(|i| state[(i+6)%9] += state[(i+8)%9]).last();
     println!("{}", state.iter().sum::<u64>());
 }
