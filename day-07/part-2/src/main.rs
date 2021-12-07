8c224d69af807879ca42b47d6ee73cebaa0b3300
Miquel van Smoorenburg <mike@langeraar.net>
2021-12-07 18:05:42 +0100

    day-07/part-2: use helper lib

--- day-07/part-2/src/main.rs
+++ day-07/part-2/src/main.rs
@@ -1,13 +1,8 @@
-fn read_csv_line<T: std::str::FromStr>() -> Vec<T> {
-    let mut line = String::new();
-    std::io::stdin().read_line(&mut line).unwrap();
-    line.trim().split(',').flat_map(|v| v.parse::<T>()).collect()
-}
-
-fn main() {
-    let crabs = read_csv_line::<i64>();
+fn main() -> aoclib::Result<()> {
+    let crabs = aoclib::read_csv_line::<i64>()?;
     let max = crabs.iter().cloned().max().unwrap();
     let psum_abs = |n: i64| -> i64 { (0i64 ..= n.abs()).sum() };
     let r = (0..max).map(|w| crabs.iter().map(|c| psum_abs(c - w)).sum::<i64>()).min();
     println!("{}", r.unwrap());
+    Ok(())
 }
