9df98b6e3d578d6a645f798b220411de6a6ec38f
Miquel van Smoorenburg <mike@langeraar.net>
2021-12-07 12:50:49 +0100

    fix up day-07/part2

--- day-07/part-2/src/main.rs
+++ day-07/part-2/src/main.rs
@@ -1,19 +1,13 @@
-fn main() {
+fn read_csv_line<T: std::str::FromStr>() -> Vec<T> {
     let mut line = String::new();
     std::io::stdin().read_line(&mut line).unwrap();
-    let numbers = line.trim().split(',').map(|v| v.parse::<i64>().unwrap()).collect::<Vec<_>>();
-    let max = *numbers.iter().max().unwrap_or(&0);
-    let mut data = Vec::new();
-    data.resize(max as usize, 0i64);
-    for n in numbers {
-        for w in 0 .. max {
-            data[w as usize] += fib((n - w).abs());
-        }
-    }
-    println!("{}", data.iter().min().unwrap());
+    line.trim().split(',').flat_map(|v| v.parse::<T>()).collect()
 }
 
-fn fib(n: i64) -> i64 {
-    (0 ..= n).fold(0i64, |accum, x| accum + x)
+fn main() {
+    let crabs = read_csv_line::<i64>();
+    let max = crabs.iter().cloned().max().unwrap();
+    let psum_abs = |n: i64| -> i64 { (0i64 ..= n.abs()).sum() };
+    let r = (0..max).map(|w| crabs.iter().map(|c| psum_abs(c - w)).sum::<i64>()).min();
+    println!("{}", r.unwrap());
 }
-
