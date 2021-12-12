b95b949430c6f612c52b5598b212894faaa0c7b6
Miquel van Smoorenburg <mike@langeraar.net>
2021-12-13 00:44:47 +0100

    simplify

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -15,32 +15,6 @@ impl Cave {
     }
 }
 
-#[derive(Clone, Default)]
-struct Path {
-    once:   HashSet<String>,
-    twice:  bool,
-    ends:   bool,
-}
-
-struct Paths(Vec<Path>);
-
-impl Paths {
-    fn new(twice: bool) -> Paths {
-        Paths(vec![ Path { twice, ..Path::default() }])
-    }
-
-    fn prune(&mut self) {
-        self.0.retain(|p| p.ends);
-    }
-
-    fn branch(&mut self, cur_path: usize, twice: bool) -> usize {
-        let mut branch = self.0[cur_path].clone();
-        branch.twice = twice;
-        self.0.push(branch);
-        self.0.len() - 1
-    }
-}
-
 #[derive(Default)]
 struct Map {
     caves:  HashMap::<String, Cave>,
@@ -57,43 +31,38 @@ impl Map {
                 map.caves.entry(c2.clone()).or_insert(Cave::new(&c2)).next.push(c1.clone());
             }
         }
-        map.caves.values_mut().for_each(|c| c.next.sort());
         map
     }
 
-    fn explore(&self, paths: &mut Paths, cur_path: usize, name: &str) {
+    fn explore(&self, name: &str, twice: bool, mut visited: HashSet<String>) -> u32 {
         if name == "end" {
-            paths.0[cur_path].ends = true;
-            return;
+            return 1;
         }
         let cave = &self.caves[name];
         if cave.small {
-            paths.0[cur_path].once.insert(name.to_string());
+            visited.insert(name.to_string());
         }
+        let mut paths = 0;
         for next in &cave.next {
-            let mut twice = paths.0[cur_path].twice;
-            if paths.0[cur_path].once.contains(next) {
+            let mut twice = twice;
+            if visited.contains(next) {
                 if !twice {
                     continue;
                 }
                 twice = false;
             }
-            let branch = paths.branch(cur_path, twice);
-            self.explore(paths, branch, next);
+            paths += self.explore(next, twice, visited.clone());
         }
+        paths
     }
 }
 
 fn main() {
     let map = Map::read_from_stdin();
 
-    let mut paths = Paths::new(false);
-    map.explore(&mut paths, 0, "start");
-    paths.prune();
-    println!("part1 # of paths: {}", paths.0.len());
+    let p1 = map.explore("start", false, HashSet::new());
+    println!("part1 # of paths: {}", p1);
 
-    let mut paths = Paths::new(true);
-    map.explore(&mut paths, 0, "start");
-    paths.prune();
-    println!("part2 # of paths: {}", paths.0.len());
+    let p2 = map.explore("start", true, HashSet::new());
+    println!("part2 # of paths: {}", p2);
 }
