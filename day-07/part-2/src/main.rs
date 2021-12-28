974e7ea52e753dd16c899d9c7931c04a4f2672ae
Miquel van Smoorenburg <mike@langeraar.net>
2021-12-28 17:48:00 +0100

    remove aoclib

--- day-07/part-2/src/main.rs
+++ day-07/part-2/src/main.rs
@@ -1,8 +1,9 @@
-fn main() -> aoclib::Result<()> {
-    let crabs = aoclib::read_csv_line::<i64>()?;
+use std::io::{self, BufRead};
+fn main() {
+    let line = io::stdin().lock().lines().next().unwrap().unwrap();
+    let crabs = line.split(',').map(|v| v.trim().parse::<i64>().unwrap()).collect::<Vec<_>>();
     let max = crabs.iter().cloned().max().unwrap();
     let psum_abs = |n: i64| -> i64 { (0i64 ..= n.abs()).sum() };
     let r = (0..max).map(|w| crabs.iter().map(|c| psum_abs(c - w)).sum::<i64>()).min();
     println!("{}", r.unwrap());
-    Ok(())
 }
